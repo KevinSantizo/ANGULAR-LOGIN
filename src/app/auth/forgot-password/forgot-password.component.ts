712e52134839e33fca79e6fbc4c46b0dfeed425a
KevinSantizo <kevinsantizo660@gmail.com>
2020-05-22 22:56:59 -0600

    Testing email sent

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -12,13 +13,20 @@ export class ForgotPasswordComponent implements OnInit {
 
   userEmail = new FormControl('');
 
-  constructor() { }
+  constructor(private authSvc: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onReset(){
-    console.log('On Reset');
-    
+  async onReset(){
+    try {
+      const email = this.userEmail.value;
+      await this.authSvc.resetPassword(email);
+      window.alert('Email sent, check your inbox');
+      //Redirect to login
+      this.router.navigate(['/login'])
+    } catch (error) {
+      console.log(error);
+    }
   }
 }

1f8879cdd1bd6536c99e8ee04326f74b28e8324b
KevinSantizo <kevinsantizo660@gmail.com>
2020-05-22 22:45:35 -0600

    Component almost works

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
+  providers: [AuthService]
 })
 export class ForgotPasswordComponent implements OnInit {
+
+  userEmail = new FormControl('');
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onReset(){
+    console.log('On Reset');
+    
+  }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -7,6 +7,15 @@ export class AuthService {
 
     constructor (public afAuth: AngularFireAuth, ){}
 
+    async resetPassword(email: string): Promise<void>{
+        try {
+            return this.afAuth.sendPasswordResetEmail(email);
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
     async snedVerificationEmail(): Promise<void>{
         return (await this.afAuth.currentUser).sendEmailVerification();
     }
